717b861744657f13262c92751c3689555e1f4e77
Oliver Mainey <ober3550@gmail.com>
2023-12-03 17:56:51 +1000

    Day2 Part2

--- Day2/run.js
+++ Day2/run.js
@@ -6,7 +6,7 @@ const maxCubes = {
   b: 14,
 };
 
-let validGames = [];
+let games = [];
 for (let i = 0; i < data.length - 1; i++) {
   // We can make assumptions about the games (information that's not valid to process)
   // We don't need the game id since that's just the line number
@@ -14,22 +14,46 @@ for (let i = 0; i < data.length - 1; i++) {
   // So per line we only need the number of cubes and the color
   // We don't even need the full name of the color since rgb are all distinct
   let picks = data[i].match(/[0-9]+ [rgb]/g);
-  let valid = true;
+  let game = {
+    id: i + 1,
+    valid: true,
+    power: 0,
+    maxes: {
+      r: 0,
+      g: 0,
+      b: 0,
+    },
+  };
   for (let j = 0; j < picks.length; j++) {
+    // Make sure to eval the count as a number not a string
     let turn = picks[j].split(" ");
+    turn[0] = Number.parseInt(turn[0]);
+    if (turn[0] > game.maxes[turn[1]]) {
+      game.maxes[turn[1]] = turn[0];
+    }
     // If a turn has more cubes than the max for a bag that bag is invalid
     // We can break early in this case
     if (turn[0] > maxCubes[turn[1]]) {
-      valid = false;
-      break;
+      game.valid = false;
     }
   }
-  if (valid) {
-    validGames.push(i + 1);
-  }
+  game.power = game.maxes.r * game.maxes.g * game.maxes.b;
+  games.push(game);
 }
 
-// We could just sum the ids while processing each line
-// but doing this allows us to check which games the
-// algorithm determined were valid so we can manually check them
-console.log(validGames.reduce((a, b) => a + b, 0));
+// Part 1
+console.log(
+  "Part 1:",
+  games
+    .filter((x) => {
+      return x.valid;
+    })
+    .map((x) => x.id)
+    .reduce((a, b) => a + b, 0)
+);
+
+// Part 2
+console.log(
+  "Part 2:",
+  games.map((x) => x.power).reduce((a, b) => a + b, 0)
+);
