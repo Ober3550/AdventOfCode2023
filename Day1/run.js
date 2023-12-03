5d25b3bf01825ab23f99a75bc33abf604041a585
Oliver Mainey <ober3550@gmail.com>
2023-12-03 19:56:48 +1000

    Day1 Part2

--- Day1/run.js
+++ Day1/run.js
@@ -3,17 +3,32 @@ const fs = require("fs");
 // Load the file input.txt
 let data = fs.readFileSync("./input.txt", { encoding: "utf8", flag: "r" });
 
+// Part 2
+if (true) {
+  data = data.replace(/(one)/g, "$11$1");
+  data = data.replace(/(two)/g, "$12$1");
+  data = data.replace(/(three)/g, "$13$1");
+  data = data.replace(/(four)/g, "$14$1");
+  data = data.replace(/(five)/g, "$15$1");
+  data = data.replace(/(six)/g, "$16$1");
+  data = data.replace(/(seven)/g, "$17$1");
+  data = data.replace(/(eight)/g, "$18$1");
+  data = data.replace(/(nine)/g, "$19$1");
+}
+
 // Remove all but the numbers
 data = data.replace(/[a-z]/g, "");
 data = data.split("\n");
 
 // Iterate over each line and grab the first and last digit to create a 2 digit number
-// Sum those numbers for your answer
-let sum = 0;
+let numbers = [];
 for (let i = 0; i < data.length - 1; i++) {
   let string = data[i];
   let first = string[0];
   let last = string.slice(-1);
-  sum += Number.parseInt(first + last);
+  numbers.push(Number.parseInt(first + last));
 }
-console.log(sum);
+console.log(
+  "Part 2:",
+  numbers.reduce((a, b) => a + b, 0)
+);
